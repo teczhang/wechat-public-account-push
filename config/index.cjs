0548269a267b67a1516b7596be7c4c800dfca7c3
teczhang <127932859+teczhang@users.noreply.github.com>
2023-03-15 16:13:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oc0xY6wBmav5tfV_8PBpeBfgz47o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
