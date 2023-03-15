2d92628c0b841adfccc46c6148c9527ac9a4bf58
teczhang <127932859+teczhang@users.noreply.github.com>
2023-03-15 16:07:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,22 +8,22 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx23d0a18ab44e6ac6',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'adafb384a671331f68301c0aed8d34e0',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '浙江',
+  CITY: '宁波',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oc0xY6wBmav5tfV_8PBpeBfgz47o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'oc0xY6wBmav5tfV_8PBpeBfgz47o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'g6ge0HqRCdXn7zJf6ZS70WEIW97JXZY9LAOrIXB0-f0',
 
   CALLBACK_USERS: [
     {
