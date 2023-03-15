c78fec78f2bcd9d91595cb53bb35c7f38b93ee18
teczhang <127932859+teczhang@users.noreply.github.com>
2023-03-15 16:11:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'g6ge0HqRCdXn7zJf6ZS70WEIW97JXZY9LAOrIXB0-f0',
     }
   ],
 
